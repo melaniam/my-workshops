d256acae791cddb78c444f8cd985912308c813da
melaniam <melania.silvia.moldovan@gmail.com>
2019-11-03 19:14:07 +0200

    Add step 9: Implement update and delete functions in state.js

--- front-end-basics/code/state.js
+++ front-end-basics/code/state.js
@@ -4,3 +4,29 @@ const addTodo = newTodo => {
     todos.push(newTodo);
     console.log("addTodo", newTodo, todos); // just for testing
 };
+
+const updateTodo = newTodo => {
+    const oldTodo = todos.find(t => t.text === newTodo.text);
+    if (oldTodo) {
+        if (oldTodo.done !== newTodo.done) {
+            oldTodo.done = newTodo.done;
+        } else {
+            console.log("todo has not changed");
+        }
+    } else {
+        console.log("todo not found");
+    }
+
+    console.log("updateTodo", newTodo, todos); // just for testing
+};
+
+const deleteTodo = todoToDelete => {
+    const index = todos.indexOf(t => t.text === todoToDelete.text);
+    if (index === -1) {
+        console.log("todo not found");
+    } else {
+        todos.splice(index, 1);
+    }
+
+    console.log("deleteTodo", todoToDelete, todos); // just for testing
+};
