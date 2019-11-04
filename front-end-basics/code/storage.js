7293948bae62fec47000beedaa21fbae33cea2d0
melaniam <melania.silvia.moldovan@gmail.com>
2019-11-04 22:07:33 +0200

    Add step 11: Implement functions that store and read data to and from the local storage

---
+++ front-end-basics/code/storage.js
@@ -0,0 +1,18 @@
+const TODOS_KEY = 'todos';
+
+const storeTodos = todos => {
+    const jsonString = JSON.stringify(todos);
+    localStorage.setItem(TODOS_KEY, jsonString);
+};
+  
+const getTodos = () => {
+    const todosAsString = localStorage.getItem(TODOS_KEY);
+    return JSON.parse(todosAsString) || [];
+};
+
+// test
+const testTodos = [{text: "todo 1", done: false}, {text: "todo 2", done: true}, {text: "todo 3", done: false}];
+storeTodos(testTodos);
+
+const storedTodos = getTodos();
+console.log("storedTodos", storedTodos);
